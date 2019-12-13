1513410b4237ab56c579f423e13b9bdcf500839a
hota1024 <hotalog@hota1024.com>
2019-12-14 00:30:00 +0900

    :globe_with_meridians: Update ja language.

--- src/locale/ja.ts
+++ src/locale/ja.ts
@@ -21,8 +21,21 @@ export const ja = {
       description: '引数をそのまま返信します。'
     }
   },
-  version: 'バージョン',
-  beta: 'ベータ',
-  prefixes: 'プレフィックス',
-  args: '引数'
+  standardPlugin: {
+    version: 'バージョン',
+    beta: 'ベータ',
+    prefixes: 'プレフィックス',
+    args: '引数',
+    usage: '使用方法',
+    description: '説明',
+    noDescriptionProvided: '説明がありません。',
+    type: '型',
+    types: {
+      Any: '任意型',
+      Boolean: '論理型',
+      Number: '数値型',
+      String: '文字列型',
+      Unknown: '不明な型'
+    }
+  }
 }
