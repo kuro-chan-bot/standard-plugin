597e3d58f08171a28c6e40722244a6bd8eb75925
hota1024 <hotalog@hota1024.com>
2019-12-13 19:07:37 +0900

    :globe_with_meridians: Create locale/ja.ts

---
+++ src/locale/ja.ts
@@ -0,0 +1,28 @@
+/**
+ * Japanese.
+ */
+export const ja = {
+  commands: {
+    help: {
+      description: 'ヘルプを表示します。',
+      args: {
+        commandName: {
+          description: '対象のコマンド'
+        }
+      }
+    },
+    ping: {
+      description: 'ピン値を計測します。'
+    },
+    raw: {
+      description: '引数をそのまま返します。'
+    },
+    reply: {
+      description: '引数をそのまま返信します。'
+    }
+  },
+  version: 'バージョン',
+  beta: 'ベータ',
+  prefixes: 'プレフィックス',
+  args: '引数'
+}
